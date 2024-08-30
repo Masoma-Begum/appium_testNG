e3c65caa71d98ecc80745f3a2f5133e7fcae177e
USER <USER@DESKTOP-NHIF91H>
2024-08-30 02:51:33 -0700

    Cross browser testing with out using .xml file

--- appium/src/test/java/appium/CrossBrowser.java
+++ appium/src/test/java/appium/CrossBrowser.java
@@ -1,6 +1,47 @@
 package appium;
 
-public class CrossBrowser {
-	
+import java.io.IOException;
 
-}
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CrossBrowser {
+      WebDriver driver;
+	   @DataProvider(name = "browsers" )
+	    public Object[][] netSpeeds() {
+	        return new Object[][] {
+	            { "Chrome" },
+	            { "EDG" }
+	         
+	        };
+	    }
+	    
+	    @Test(testName = "testSpeed", dataProvider =  "browsers")
+	    public void testSpeed(String browser) throws IOException{
+	    	switch (browser.toLowerCase()) {
+			case "chrome":
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+				break;
+			case "edge":
+				WebDriverManager.edgedriver().setup();
+				driver = new EdgeDriver();
+				break;
+			default:
+				System.out.println("Browsername is invalid");
+				break;
+			}
+	    	driver.get("http://facebook.com");
+	        }
+	            
+	       
+	        
+	    
+	    }
+
+
